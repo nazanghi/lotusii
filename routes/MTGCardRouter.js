aaf83af5759708d2f00f871323a83e0d00238ce1
Nicola Zanghi <nico.zang7@gmail.com>
2020-11-19 20:46:45 -0500

    installs mongoose, jsonwebtoken, dotenv, removes obsolete/broken routes in MTGCardRouter

--- routes/MTGCardRouter.js
+++ routes/MTGCardRouter.js
@@ -3,9 +3,6 @@ const MTGCardController = require('../controllers/MTGCardController')
 
 Router.get('/:card_id', MTGCardController.GetCard)
 Router.get('/', MTGCardController.GetAllCards)
-Router.put('/:deck_id/:card_id', MTGCardController.AddCardToDeck)
-Router.delete('/:deck_id/:card_id', MTGCardController.RemoveCard)
-Router.put('/cart/:card_id', MTGCardController.AddLooseCard)
 module.exports = Router
 
 //review these functions
