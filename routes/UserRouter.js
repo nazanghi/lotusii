0944e75ed7855a61a9478821233193bfb3e7b3de
Nicola Zanghi <nico.zang7@gmail.com>
2020-11-23 05:52:30 -0500

    fixes RefreshSession 404 error

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -9,5 +9,10 @@ const {
 Router.get('/:user_id', UserController.GetProfile)
 Router.post('/register', UserController.CreateUser)
 Router.post('/login', UserController.SignInUser, createToken)
-
+Router.get(
+    '/refresh/session',
+    getToken,
+    verifyToken,
+    UserController.RefreshSession
+)
 module.exports = Router
